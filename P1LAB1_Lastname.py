8ebaec11280300199d5b2c11cc38cb6127196300
Andrew Norris <norrisa@faytechcc.edu>
2021-09-16 11:08:27 -0400

    First upload

---
+++ P1LAB1_Lastname.py
@@ -0,0 +1,11 @@
+# CTI 110
+# P1LAB1
+# Name 
+# Date     
+
+# This is 7.12 from Zybooks
+
+userNum = int(input())
+userNumSquared = userNum * userNum   # Math Bug fixed
+   
+print(userNumSquared)       # Output formatting issue fixed
